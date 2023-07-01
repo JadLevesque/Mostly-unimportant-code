ec894c7949ab47ba37445b667e6d283f6cc887d1
JadLévesque <30678191+JadLevesque@users.noreply.github.com>
2023-07-01 18:30:30 -0400

    Update main.c
    Improvements:
    - Removed useless definitions
    - Reorganised the useful definitions
    - Streamlined the definition of `P`

--- fib stack file iter/main.c
+++ fib stack file iter/main.c
@@ -1,4 +1,4 @@
-#4294967256
+#4294967257
 /**
 Did you know that one in every billion comments is important to the 
 compilation of a program?... 
@@ -11,27 +11,29 @@ of Fibonacci elements printed on the output.
 #ifndef stdin
     #0"stdio.h"3
     #include __FILE__ //""
-    int main () {}
 
-    #define PCAT(a,b) a##b
-    #define CAT(x...) PCAT (x)
-    #define P __attribute__((constructor)) void CAT(f,CN) () {puts (&__func__[1]);} 
-    #define BF __BASE_FILE__
-    #define IL __INCLUDE_LEVEL__
-    #define LN __LINE__
-    #define CN __COUNTER__
-    
     #define PRAGMA(p) _Pragma(#p)
     #define PH(m) PRAGMA (push_macro(#m))
     #define PP(m) PRAGMA (pop_macro(#m))
 
-    #define EAT(...)
-    #define DUMP(xs...) EAT (xs)
+    #define PCAT(a,b) a##b
+    #define CAT(x...) PCAT (x)
 
     #define IF_(t,f...) f
     #define IF_1(t,f...) t
-
+    
     #define ONEP(x,xs...) IF_##__VA_OPT__(1)
+    
+    #define EAT(...)
+    #define DUMP(xs...) EAT (xs)
+
+    #define IL __INCLUDE_LEVEL__
+    #define LN __LINE__
+    #define CN __COUNTER__
+    
+    #define P __attribute__((constructor)) void CAT (f, CN) () {puts (&__func__[1]);} 
+    PH (P)
+    #define P PP (P) int main () {DUMP (CN)}
 
     #define A(...) 
     #define B(...)
@@ -40,34 +42,24 @@ of Fibonacci elements printed on the output.
     PH (B)
     PH (C)
 
-    #undef B
-    #undef C
-
     #define B(...) PP (B) B (.) __VA_OPT__ (PH (C) PH (A))
     #define C(...) PP (C) C (.) __VA_OPT__ (PH (B))
 
     PH (B)
 #endif
 #if IL <= LN
-    #if IL
-        P
-    #else
-        DUMP (CN)
-    #endif
+    P
 
     A ()
     PH (A)
-    #undef A
     #define A(xs...) PP (A) A (.,##xs) __VA_OPT__ (PH (C) ONEP (xs) (DUMP (CN)))
 
     B ()
     PH (B)
-    #undef B
     #define B(...) PP (B) B (.) __VA_OPT__ (PH (C) PH (A))
 
     C ()
     PH (C)
-    #undef C
     #define C(...) PP (C) C (.) __VA_OPT__ (PH (B))
 
     #include __BASE_FILE__ //""
